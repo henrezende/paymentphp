2fffb43e676e8586b5d89fc9024b582c7918e3c3
Henrique Rezende <henrique.64@hotmail.com>
2024-02-21 19:19:59 -0300

    feat: routes to index and store payments

--- api/routes/api.php
+++ api/routes/api.php
@@ -17,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('payments')->group(function () {
+    Route::get('/', 'App\Http\Controllers\PaymentController@index');
+    Route::post('/', 'App\Http\Controllers\PaymentController@store');
+    // Route::get('/{id}', 'App\Http\Controllers\PaymentController@show');
+    // Route::put('/{id}', 'App\Http\Controllers\PaymentController@update');
+    // Route::delete('/{id}', 'App\Http\Controllers\PaymentController@destroy');
+});
