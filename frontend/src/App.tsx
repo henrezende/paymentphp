7065111259261673cfb0ef808d53563071bf4a06
Henrique Rezende <henrique.64@hotmail.com>
2024-02-24 17:36:14 -0300

    feat: removed unnecessary lines

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,61 +1,3 @@
-// import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
-// import "./App.css";
-// import { initMercadoPago, getInstallments } from "@mercadopago/sdk-react";
-
-// function App() {
-//   const [count, setCount] = useState(0);
-//   initMercadoPago("TEST-b1968cca-0d6c-4498-a7c3-854d5901c8f8");
-
-//   const fetchInstallments = async () => {
-//     try {
-//       const installments = await getInstallments({
-//         amount: "10000",
-//         locale: "pt-BR",
-//         bin: "5031433215406351",
-//       });
-
-//       const installments2 = await getInstallments({
-//         amount: "32423",
-//         bin: "5031433215406351",
-//         paymentTypeId: "credit_card",
-//       });
-
-//       console.log("Installments:", installments);
-//       console.log("Installments2:", installments2);
-//     } catch (error) {
-//       console.error("Error getting installments:", error);
-//     }
-//   };
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vitejs.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1> Vite + React </h1>
-//       <div className="card">
-//         <button onClick={fetchInstallments}>count is {count}</button>
-//         <p>
-//           Edit <code> src / App.tsx </code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   );
-// }
-
-// export default App;
-
-import React from 'react';
 import {
   Route,
   createBrowserRouter,
@@ -72,7 +14,7 @@ const router = createBrowserRouter(
   )
 );
 
-function App({ routes }) {
+function App() {
   return (
     <>
       <RouterProvider router={router} />
